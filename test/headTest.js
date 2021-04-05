92db150ebfa9f5a858778f29d6c984fd64a7b975
Brandon <bmacdonald1986@gmail.com>
2021-04-05 20:42:46 +0000

    refactored headTest, refactored middleTest

--- test/headTest.js
+++ test/headTest.js
@@ -4,27 +4,27 @@ const head = require('../head');
 describe('#head', ()=>{
 
   it('returns 1 for [1, 2, 3]', ()=>{
-    assert.equal(head([1,2,3]), 1);
+    assert.strictEqual(head([1,2,3]), 1);
   });
 
   it("returns 5 for ['5']", ()=>{
-    assert.equal(head(['5']), '5');
+    assert.strictEqual(head(['5']), '5');
   });
 
   it("returns 'Red' for ['Red', 'Green', 'Blue]", ()=>{
-    assert.equal(head(['Red', 'Green', 'Blue']), 'Red');
+    assert.strictEqual(head(['Red', 'Green', 'Blue']), 'Red');
   });
 
   it("returns true for []", ()=>{
-    assert.equal(head([]), true);
+    assert.strictEqual(head([]), true);
   });
 
   it("returns true for [true]", ()=>{
-    assert.equal(head([true]), true);
+    assert.strictEqual(head([true]), true);
   });
 
   it("returns 0 for 'a'", ()=>{
-    assert.equal(head('a'), 0);
+    assert.strictEqual(head('a'), 0);
   });
 
 });
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,22 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-//Test case
-assertArraysEqual(middle([1,2,3,4]),[2,3]);
-assertArraysEqual(middle([1,2,3,4,5]),[3]);
-assertArraysEqual(middle([1,2,3,4,5,6,7]),[4]);
-assertArraysEqual(middle([1,2,3,4,5,6,7,8]),[4,5]);
+describe('#head', ()=>{
+  
+  it('should return [2,3] when [1,2,3,4] is passed', ()=>{
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+
+  it('should return [3] when [1,2,3,4,5] is passed', ()=>{
+    assert.deepEqual(middle([1,2,3,4,5]), [3]);
+  });
+
+  it('should return [4] when [1,2,3,4,5,6,7] is passed', ()=>{
+    assert.deepEqual(middle([1,2,3,4,5,6,7]), [4]);
+  });
+
+  it('should return [4,5] when [1,2,3,4,5,6,7,8] is passed', ()=>{
+    assert.deepEqual(middle([1,2,3,4,5,6,7,8]), [4,5]);
+  });
+
+});
