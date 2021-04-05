b46a6edd3ed4a4370d640dd0b5f8b0b1c147443a
Brandon <bmacdonald1986@gmail.com>
2021-04-05 20:34:07 +0000

    refactored head and headTest to use mocha and chai packages

--- head.js
+++ head.js
@@ -1,13 +1,19 @@
 const head = function(arr) {
-  if (arr) {
-    if (Array.isArray(arr)) {
-      return arr[0];
-    } else {
-      return undefined;
-    }
-  } else {
+
+  if (!arr) {
     return undefined;
   }
+
+  if (!Array.isArray(arr)) {
+    return 0;
+  }
+  
+  if (arr.length < 1) {
+    return true;
+  }
+
+  return arr[0];
+
 };
 
 module.exports = head;
--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,30 @@
-const assertEqual = require('./assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
-// Test Code
-assertEqual(head([5,6,7,8]), 5);
-assertEqual(head(["Red", "Green", "Yellow"]), "Blue");
-assertEqual(head([]), true);
-assertEqual(head([true]), true);
-assertEqual(head('a'), 0);
+
+describe('#head', ()=>{
+
+  it('returns 1 for [1, 2, 3]', ()=>{
+    assert.equal(head([1,2,3]), 1);
+  });
+
+  it("returns 5 for ['5']", ()=>{
+    assert.equal(head(['5']), '5');
+  });
+
+  it("returns 'Red' for ['Red', 'Green', 'Blue]", ()=>{
+    assert.equal(head(['Red', 'Green', 'Blue']), 'Red');
+  });
+
+  it("returns true for []", ()=>{
+    assert.equal(head([]), true);
+  });
+
+  it("returns true for [true]", ()=>{
+    assert.equal(head([true]), true);
+  });
+
+  it("returns 0 for 'a'", ()=>{
+    assert.equal(head('a'), 0);
+  });
+
+});
