820da84787ab36d9e0d2ca1472d5b21510a3145e
Brandon <bmacdonald1986@gmail.com>
2021-04-05 20:16:58 +0000

    added assertArraysEqualTest and refactored assertArraysEqual

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,6 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+//Test case
+assertArraysEqual([1,2],[1,2,3]);
+assertArraysEqual(["1","2","3"],[1,2,3]);
+assertArraysEqual([1,2,3],[1,2,3]);
